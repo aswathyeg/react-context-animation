614086ecbcfe3b9041072112e1ec6675fffe43dd
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-27 15:21:53 +0530

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <>
+        <h1>
+            Your Challenges
+        </h1>
+        <button>Add Challenge</button>
+        </>
+    )
+
+}
