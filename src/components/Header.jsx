ee9c857ef5e5abc223849c887efb57f09bf93b88
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-04-01 06:39:30 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
+import NewChallenge from './NewChallenge';
+import { useState } from "react"
 export default function Header(){
+    const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
+    function handleStartAddNewChallenge() {
+        setIsCreatingNewChallenge(true);
+      }
+      function handleDone() {
+        setIsCreatingNewChallenge(false);
+      }
     return(
         <>
-        <h1>
-            Your Challenges
-        </h1>
-        <button>Add Challenge</button>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+        <header id="main-header">
+               <h1 > Your Challenges</h1>
+        <button className="buttonAdd" 
+        onClick={handleStartAddNewChallenge}
+        >Add Challenge</button>
+        
+        </header>
         </>
     )
 
